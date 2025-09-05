cb78c8d33ee74427d05c8cf51bd2790eae3f3030
StivenArroyave05 <stiven.arroyave05@gmail.com>
2025-09-04 22:12:21 -0500

    Initial commit: preparar para Netlify

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    './style.css'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        starBlue: '#60a5fa',
+        starYellow: '#facc15',
+        starGreen: '#4ade80',
+        starPink: '#f472b6',
+        starPurple: '#c084fc',
+      },
+    },
+  },
+  plugins: [],
+};
---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+import tailwindPostcss from '@tailwindcss/postcss';
+import autoprefixer from 'autoprefixer';
+
+export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwindPostcss(), autoprefixer()],
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+});
