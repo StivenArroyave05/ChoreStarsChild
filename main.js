7d2050a2fe50115fffb41109a21f0136542a46d6
FacturaFácilPyme <stiven.arroyave05@gmail.com>
2025-09-08 15:42:10 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // main.js
-const APP_VERSION = "1.0.2";  // Actualízalo en cada release
+const APP_VERSION = "1.0.01";  // Actualízalo en cada release
 
 // 1) Traducciones
 const translations = {
@@ -2074,4 +2074,5 @@ function sendSkipWaiting(worker) {
     }
   });
 }
+
 
